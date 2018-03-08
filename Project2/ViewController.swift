68f5297b0722b480c1ca71b579e560755526a88c
Rodney Simba Masarirambi <aethersven@icloud.com>
2018-03-08 15:16:47 -0600

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Rodney Simba Masarirambi on 3/8/18.
+//  Copyright © 2018 Rodney Simba Masarirambi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
